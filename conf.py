96f8d7029d808f6702dc11fedc8d3799214197ec
Semidio <Semidio@users.noreply.github.com>
2019-12-24 19:16:35 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 enable_jsdelivr = {
-    "enabled": False,
+    "enabled": True,
     "repo": ""
 }
 
@@ -17,22 +17,17 @@ enable_jsdelivr = {
 site_name = "我的个人博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "Semidio"
+email = "semidio7#gmail.com"
+author_homepage = "https://www.xxx.xx"
+description = "XXXXXX"
+key_words = ['Maverick', 'Semidio', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
         "name": "Maverick",
         "url": "https://github.com/AlanDecode/Maverick",
         "brief": "🏄‍ Go My Own Way."
-    },
-    {
-        "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
     }
 ]
 nav = [
@@ -56,17 +51,17 @@ nav = [
 social_links = [
     {
         "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
+        "url": "https://twitter.com/Semidio7",
         "icon": "gi gi-twitter"
     },
     {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/Semidio",
         "icon": "gi gi-github"
     },
     {
         "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
+        "url": "https://weibo.com/semidio/",
         "icon": "gi gi-weibo"
     }
 ]
