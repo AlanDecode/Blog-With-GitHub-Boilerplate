5bb3d88fb6de0b84e512f0397e48c827d5d543d7
liluoyi1234 <30952504+liluoyi1234@users.noreply.github.com>
2019-12-25 19:39:56 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,14 +14,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "L的个人博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "L"
+email = "liluoyi1234@gmail.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+description = "突破，无止境"
+key_words = [ 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
@@ -32,7 +32,7 @@ external_links = [
     {
         "name": "三無計劃",
         "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
+        "brief": "L的主页。"
     }
 ]
 nav = [
