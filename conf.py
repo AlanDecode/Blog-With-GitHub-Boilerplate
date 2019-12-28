03b2bfa93f31c4e400330c74789ff03b4f62c1ac
MrNiuEight <708951773@qq.com>
2019-12-28 20:03:11 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,8 +9,8 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "mrniueight/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
