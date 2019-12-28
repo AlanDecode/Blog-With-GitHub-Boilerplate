4776b51f7f5ba0080d7e73d30f22a1c14aaaeac9
ixxmu <ixxmu@users.noreply.github.com>
2019-12-28 16:32:17 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,14 +14,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "小可的博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "kk"
+email = "kkitown@gmail.com"
+author_homepage = "https://twitter.com/kkitown"
+description = "瞳孔放大不代表就能看的更多"
+key_words = ['kkitown', 'R', 'ixxmu', 'blog','生信']
 language = 'zh-CN'
 external_links = [
     {
