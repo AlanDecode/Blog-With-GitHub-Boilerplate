e6970f1bc25ff9785b12ddcfb493ef54ee677778
Arley517693777 <55181721+Arley517693777@users.noreply.github.com>
2020-02-12 13:19:41 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -28,6 +28,7 @@ author_homepage = "https://www.arley.cn"
 description = "Living without an aim is like sailing without a compass。<br>生活没有目标，犹如航海没有罗盘。"
 key_words = ['Maverick', 'arley', 'Galileo', 'blog']
 language = 'zh-CN'
+background_img = "${static_prefix}bg.png"
 
 nav = [
     {
