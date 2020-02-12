8f7ecfd614874017d356074047bbbd8f6e46cab1
Arley517693777 <55181721+Arley517693777@users.noreply.github.com>
2020-02-12 23:16:26 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ enable_jsdelivr = {
 
 # 站点设置
 site_name = "Arley's Blog"
-site_logo = "${static_prefix}logo.png"
+site_logo = "${static_prefix}Arley.png"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "Arley"
 email = "arley@arley.cn"
