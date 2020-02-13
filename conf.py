87a37e1f32c26b6283fdc5da6a09d2935f4724d4
Arley517693777 <55181721+Arley517693777@users.noreply.github.com>
2020-02-13 13:08:33 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -43,6 +43,11 @@ nav = [
         "target": "_self"
     },
     {
+        "name": "读书",
+        "url": "${site_prefix}read/",
+        "target": "_self"
+    },
+    {
         "name": "归档",
         "url": "${site_prefix}archives/",
         "target": "_self"
