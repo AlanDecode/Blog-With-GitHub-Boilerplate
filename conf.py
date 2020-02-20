9ecc4a2f1f7c6606bcbe1a5d561663439781b67e
lvning <lvning2012@gmail.com>
2020-02-20 17:07:28 +0800

    update

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ enable_jsdelivr = {
 # 站点设置
 site_name = "PeterFox"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
+site_build_date = "2020-02-20T16:50+08:00"
 author = "PeterFox"
 email = "tingbob@hotmail.com"
 author_homepage = ""
