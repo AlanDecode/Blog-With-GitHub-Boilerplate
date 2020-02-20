1f9d150774dae86f70d2cf8134118fe025bdf4bc
tingbob <tingbob@hotmail.com>
2020-02-20 09:41:03 +0800

    Update conf.py
    update

--- conf.py
+++ conf.py
@@ -19,26 +19,16 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "PeterFox"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "PeterFox"
+email = "tingbob@hotmail.com"
+author_homepage = ""
+description = "谁家玉笛暗飞声，散入春风满洛城。"
+key_words = ['Maverick', 'PeterFox', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
-    {
-        "name": "Maverick",
-        "url": "https://github.com/AlanDecode/Maverick",
-        "brief": "🏄‍ Go My Own Way."
-    },
-    {
-        "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
-    }
 ]
 nav = [
     {
@@ -60,19 +50,9 @@ nav = [
 
 social_links = [
     {
-        "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
-        "icon": "gi gi-twitter"
-    },
-    {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/tingbob",
         "icon": "gi gi-github"
-    },
-    {
-        "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
-        "icon": "gi gi-weibo"
     }
 ]
 
