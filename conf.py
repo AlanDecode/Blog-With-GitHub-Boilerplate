b3902fbabb32cda896c8232f1934ddb75c155ad4
liangtyx <62633941+liangtyx@users.noreply.github.com>
2020-03-25 13:23:29 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "腾曦Tech"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "Open"
+email = "liangtyx@gmail.com"
+author_homepage = "https://liangtyx.github.io"
+description = "填不满的半杯水"
+key_words = ['Maverick', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
