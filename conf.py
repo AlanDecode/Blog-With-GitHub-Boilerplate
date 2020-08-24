d14da6b83d7931d2264fb115a559e839b84d4765
张文涛 <1523339047@qq.com>
2020-08-24 21:02:01 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -22,11 +22,11 @@ enable_jsdelivr = {
 site_name = "我的个人博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "张文涛"
+email = "1523339047@qq.com"
 author_homepage = "https://www.imalan.cn"
 description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+key_words = ['Maverick', '张文涛', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
@@ -66,7 +66,7 @@ social_links = [
     },
     {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/zwtdgithub",
         "icon": "gi gi-github"
     },
     {
