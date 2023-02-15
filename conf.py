361809da815e1c8f12ab142ef23ec6bd0e29e4c0
wenxinyuan <48885367+wenxinyuan@users.noreply.github.com>
2023-02-15 16:06:42 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,13 +19,13 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "浮生六记"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "waker"
+email = "walkers_xy@163.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+description = "日常记录"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
